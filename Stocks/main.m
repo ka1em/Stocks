fe28723ef15aceb3920ded861034938bbc283dda
ka1em <ka1em@ka1emtekiMac.local>
2016-07-18 19:33:26 +0800

    Initial Commit

---
+++ Stocks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by ka1em on 16-7-18.
+//  Copyright (c) 2016年 Ka1emApp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
